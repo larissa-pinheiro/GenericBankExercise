12514c64209a973f5cc816cef8de715c1399ab9c
Larissa Pinheiro <rissapins@gmail.com>
2025-02-17 16:07:01 -0300

    feat: create bank and link to clients

---
+++ Entity/Bank.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBankExercise.Entity
+{
+    public class Bank
+    {
+        public string BankName { get; set; }
+        public string BankCode { get; set; }
+        public List<Client> Clients { get; set; }
+        public Bank() 
+        {
+            BankName = "Generic Bank";
+            BankCode = "937";
+            Clients = new List<Client>();
+        }
+
+        public void AddClient(Client client)
+        {
+            Clients.Add(client);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,24 +6,53 @@ namespace GenericBank
     {
         public static void Main()
         {
-            var client1 = new Client(1)
+            var bank = new Bank();
+
+            var clients = new List<Client>
             {
-                FirstName = "João",
-                LastName = "Silva",
-                BirthDate = new DateTime(1999, 3, 5),
-                Address = "Rua Ciclana, 19",
-                City = "São Paulo",
-                State = "SP",
-                ZipCode = "12345-678",
-                Phone = "(11) 00000-4121",
-                Email = "joao@gmail.com",
-                Password = "123456",
-                Balance = 0.0F,
-                Salary = 1500.0F
+                new Client(1)
+                {
+                    FirstName = "João",
+                    LastName = "Silva",
+                    BirthDate = new DateTime(1999, 3, 5),
+                    Cpf = "123.456.789-00",
+                    Address = "Rua Ciclana, 19",
+                    City = "São Paulo",
+                    State = "SP",
+                    ZipCode = "12345-678",
+                    Phone = "(11) 00000-4121",
+                    Email = "joao@gmail.com",
+                    Password = "123456",
+                    Balance = 0.0F,
+                    Salary = 1500.0F
+                },
+                new Client(2)
+                {
+                    FirstName = "Maria",
+                    LastName = "Santos",
+                    BirthDate = new DateTime(1995, 7, 15),
+                    Cpf = "987.654.321-00",
+                    Address = "Av. Fulano, 123",
+                    City = "Rio de Janeiro",
+                    State = "RJ",
+                    ZipCode = "12345-678",
+                    Phone = "(21) 00000-2920",
+                    Email = "maria@gmail.com",
+                    Password = "789101",
+                    Balance = 0.0F,
+                    Salary = 2000.0F
+                }
+
             };
 
-            Console.WriteLine($"Welcome to Generic Bank, {client1.FirstName}!");
-            Console.WriteLine($"Your balance is {client1.FormatBalance()}.");
+            foreach (var client in clients)
+            {
+                bank.AddClient(client);
+            }
+
+
+            Console.WriteLine($"Banco: {bank.BankName} ({bank.BankCode})");
+            Console.WriteLine($"Total de clientes: {bank.Clients.Count}");
         }
     }
 }
