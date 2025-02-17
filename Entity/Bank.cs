8a57493d0bbd60f82aca59a5652753eb0067f909
Larissa Pinheiro <rissapins@gmail.com>
2025-02-17 17:39:00 -0300

    clean generic uses

--- Entity/Bank.cs
+++ Entity/Bank.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GenericBankExercise.Entity
+﻿namespace GenericBankExercise.Entity
 {
     public class Bank
     {
