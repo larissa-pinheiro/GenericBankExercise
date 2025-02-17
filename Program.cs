6ab0d6dc26ad62d586d79a22da71a60180d7e68f
Larissa Pinheiro <rissapins@gmail.com>
2025-02-17 12:22:14 -0300

    create first client

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace GenericBank
+﻿using GenericBankExercise.Entity;
+
+namespace GenericBank
 {
     public static class Program
     {
         public static void Main()
         {
-            Console.WriteLine("Welcome to Generic Bank!");
+            var client1 = new Client(1)
+            {
+                FirstName = "João",
+                LastName = "Silva",
+                BirthDate = new DateTime(1999, 3, 5),
+                Address = "Rua Ciclana, 19",
+                City = "São Paulo",
+                State = "SP",
+                ZipCode = "12345-678",
+                Phone = "(11) 00000-4121",
+                Email = "joao@gmail.com",
+                Password = "123456",
+                Balance = 0.0F,
+                Salary = 1500.0F
+            };
+
+            Console.WriteLine($"Welcome to Generic Bank, {client1.FirstName}!");
+            Console.WriteLine($"Your balance is {client1.FormatBalance()}.");
         }
     }
 }
